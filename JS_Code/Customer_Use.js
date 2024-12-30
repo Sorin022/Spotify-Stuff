b742e54af2bfea5fc2a155fbc48712e8065d4f32
SorinMac <sorinmagnusmacaluso@gmail.com>
2024-12-30 13:54:08 -0500

    12/30/24 adds the song see to do list for the rest

--- JS_Code/Customer_Use.js
+++ JS_Code/Customer_Use.js
@@ -1,8 +1,9 @@
+
 const clientId = 'cc1a06dbd5944825803ef395fadc5bbd';
 const redirectUri = 'https://sorin022.github.io/Spotify-Stuff/'; // Your app’s public URL
 const scopes = 'user-library-modify playlist-read-private';
 const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
-  redirectUri
+redirectUri
 )}&response_type=token&scope=${encodeURIComponent(scopes)}`;
 
 window.location.href = authUrl; // Redirect the user
@@ -12,7 +13,7 @@ const getTokenFromUrl = () => {
     const params = new URLSearchParams(hash.substring(1));
     return params.get('access_token');
 };
-  
+
 const accessToken = getTokenFromUrl();
 console.log("Access Token: ", accessToken);
 
