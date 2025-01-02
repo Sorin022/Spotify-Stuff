ad129881e88240a278a98ef33343732b05aba757
SorinMac <sorinmagnusmacaluso@gmail.com>
2025-01-01 19:32:10 -0500

    1/1/24 testing it again

--- JS_Code/Customer_Use.js
+++ JS_Code/Customer_Use.js
@@ -9,4 +9,21 @@ function customer_login() {
     
     const accessToken = getTokenFromUrl();
     console.log("Access Token: ", accessToken);
+
+    fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'PUT',
+    headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ ids: ['3n3Ppam7vgaVa1iaRUc9Lp'] }),
+    })
+    .then((response) => {
+        if (response.ok) {
+        console.log('Song added successfully!');
+        } else {
+        console.error('Failed to add song:', response.statusText);
+        }
+    })
+    .catch(console.error);
 }
