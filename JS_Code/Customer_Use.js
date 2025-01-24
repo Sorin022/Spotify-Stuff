d709be3b6f211595ef5f60117693da56a0c288a0
SorinMac <sorinmagnusmacaluso@gmail.com>
2025-01-24 11:13:14 -0500

    1/24/25 alan told me so

--- JS_Code/Customer_Use.js
+++ JS_Code/Customer_Use.js
@@ -1,16 +1,16 @@
 function customer_login() {
     window.location.href = authUrl; // Redirect the user
 
-    const getTokenFromUrl = () => {
+    const getTokenFromUrl = () => { // function for getting the token from the spotify login
         const hash = window.location.hash;
         const params = new URLSearchParams(hash.substring(1));
         return params.get('access_token');
     };
     
-    const accessToken = getTokenFromUrl();
-    console.log("Access Token: ", accessToken);
+    const accessToken = getTokenFromUrl(); //getting the token and setting the global
+    console.log("Access Token: ", accessToken); //output for error checking
 
-    fetch('https://api.spotify.com/v1/me/tracks', {
+    fetch('https://api.spotify.com/v1/me/tracks', {  //puts a song in your liked playlist for the proof of concepts
     method: 'PUT',
     headers: {
         Authorization: `Bearer ${accessToken}`,
--- JS_Code/global.js
+++ JS_Code/global.js
@@ -1,6 +1,6 @@
-const clientId = 'cc1a06dbd5944825803ef395fadc5bbd';
-const redirectUri = 'https://sorin022.github.io/Spotify-Stuff/'; // Your app’s public URL
-const scopes = 'user-library-modify playlist-read-private';
+const clientId = 'cc1a06dbd5944825803ef395fadc5bbd'; //needed for the developer account
+const redirectUri = 'https://sorin022.github.io/Spotify-Stuff/'; // public url
+const scopes = 'user-library-modify playlist-read-private'; //needed to tell what spotify api's are going to be used
 const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
   redirectUri
-)}&response_type=token&scope=${encodeURIComponent(scopes)}`;
+)}&response_type=token&scope=${encodeURIComponent(scopes)}`; //redirect url for the loging and such
