c7465d9eed441701a07cbaace057bdcd4c1a0ee7
SorinMac <sorinmagnusmacaluso@gmail.com>
2025-01-30 19:31:21 -0500

    1/30/25 somthing is wrong with my vs code?

--- JS_Code/Customer_Use.js
+++ JS_Code/Customer_Use.js
@@ -1,31 +1,21 @@
 function customer_login() {
-    window.location.href = authUrl; // Redirect the user
-
-    const getTokenFromUrl = () => { // function for getting the token from the spotify login
-        let hash = window.location.hash;
-        let params = new URLSearchParams(hash.substring(1));
-        return params.get('access_token');
-    };
-    
-    access_token = getTokenFromUrl(); //getting the token and setting the global
-    console.log("Access Token: ", access_token); //output for error checking
-
-    XML_SpotifySong();
-
-    fetch('https://api.spotify.com/v1/me/tracks', {  //puts a song in your liked playlist for the proof of concepts
-    method: 'PUT',
-    headers: {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ ids: ['4A65rF6f8pCaAvYqgcfaWN'] }),
-    })
-    .then((response) => {
-        if (response.ok) {
-        console.log('Song added successfully!');
-        } else {
-        console.error('Failed to add song:', response.statusText);
-        }
-    })
-    .catch(console.error);
+    window.location.href = authUrl; // Redirects user to Spotify login
 }
+
+function getTokenFromUrl() {
+    let hash = window.location.hash; //the url
+    let params = new URLSearchParams(hash.substring(1)); //looking for the hash
+    return params.get('access_token'); //returns the spotify token
+}
+
+// Check for the token when the page loads
+window.onload = function () {
+    access_token = getTokenFromUrl(); //Get the token after redirect
+
+    if (access_token) { //if it get the token then show
+        console.log("Access Token:", access_token);
+        XML_SpotifySong();
+    } else { //if not found then output this
+        console.log("No access token found. Please log in.");
+    }
+};
