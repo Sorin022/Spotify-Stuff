d96879451d4608b7dc12b4b2319967ea7fd41d81
SorinMac <sorinmagnusmacaluso@gmail.com>
2025-01-30 20:44:13 -0500

    1/30/25 calling it for a night need to add a few things and then its done

--- JS_Code/XML_SpotifyID.js
+++ JS_Code/XML_SpotifyID.js
@@ -1,10 +1,3 @@
 function XML_SpotifySong() { //where the data will be processed into objects
-    //regular expression generated from chatgbt
-    const match = xml_file.match(/<key>Name<\/key>\s*<string>(?<name>.*?)<\/string>.*?<key>Artist<\/key>\s*<string>(?<artist>.*?)<\/string>.*?<key>Album<\/key>\s*<string>(?<album>.*?)<\/string>/s);
     
-    if (match) {
-        console.log("Name:", match.groups.name);
-        console.log("Artist:", match.groups.artist);
-        console.log("Album:", match.groups.album);
-    }
 }
