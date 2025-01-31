5ca92278e46032abf05358f43dd5c4ba9090fb57
SorinMac <sorinmagnusmacaluso@gmail.com>
2025-01-30 20:51:46 -0500

    1/30/25 forgot that it redirects me back to the github website hosted

--- JS_Code/XML_SpotifyID.js
+++ JS_Code/XML_SpotifyID.js
@@ -1,3 +1,25 @@
 function XML_SpotifySong() { //where the data will be processed into objects
-    
+    if(xml_file !== null){
+
+        //all by chatgbt a special xml parser
+        const parser = new DOMParser();
+
+        const xmlDoc = parser.parseFromString(xml_file, "application/xml");
+
+        // Get all <dict> elements inside <Tracks>
+        const trackDicts = xmlDoc.querySelector("key:contains('Tracks') + dict").children;
+
+        for (let i = 0; i < trackDicts.length; i++) {
+            const track = trackDicts[i].querySelector("dict");
+            if (track) {
+                const name = track.querySelector("key:contains('Name') + string")?.textContent || "Unknown";
+                const artist = track.querySelector("key:contains('Artist') + string")?.textContent || "Unknown";
+                const album = track.querySelector("key:contains('Album') + string")?.textContent || "Unknown";
+
+                console.log(`Name: ${name}`);
+                console.log(`Artist: ${artist}`);
+                console.log(`Album: ${album}`);
+            }
+        }
+    }
 }
