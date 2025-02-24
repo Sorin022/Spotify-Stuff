2d580fd0eb4414fa7b45cbd6270065da86be4218
SorinMac <sorinmagnusmacaluso@gmail.com>
2025-02-24 17:41:26 -0500

    try this out

--- JS_Code/XML_SpotifyID.js
+++ JS_Code/XML_SpotifyID.js
@@ -68,9 +68,46 @@ async function XML_SpotifySong() {
 async function createPlaylistAndAddSongs(songList) {
     let accessToken = sessionStorage.getItem("token");
 
-    /*try {
-        // 1. Get User ID
-        let userResponse = await fetch("https://api.spotify.com/v1/me", {
+    // 1. Get User ID
+    let userResponse = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+        },
+    });
+
+    let userData = await userResponse.json();
+    let userId = userData.id;
+    console.log("User ID:", userId);
+    console.log("Access Token:", accessToken);
+
+    // 2. Create Playlist
+    let playlistResponse = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            name: "Imported Songs",
+            description: "A playlist for imported songs",
+            public: false, // Set to true if you want it public
+        }),
+    });
+
+    let playlistData = await playlistResponse.json();
+    let playlistId = playlistData.id;
+    console.log("Playlist Created:", playlistData.name);
+
+    // 3. Search for Tracks and Add to Playlist
+    let trackUris = [];
+
+    for (let song of songList) {
+        let query = `track:${song.name} album:${song.album} artist:${song.artist}`;
+        let encodedQuery = encodeURIComponent(query);
+
+        let searchResponse = await fetch(`https://api.spotify.com/v1/search?q=${encodedQuery}&type=track&limit=1`, {
             method: "GET",
             headers: {
                 Authorization: `Bearer ${accessToken}`,
@@ -78,87 +115,47 @@ async function createPlaylistAndAddSongs(songList) {
             },
         });
 
-        let userData = await userResponse.json();
-        let userId = userData.id;
-        console.log("User ID:", userId);
-        console.log("Access Token:", accessToken);
+        let searchData = await searchResponse.json();
 
-        // 2. Create Playlist
-        let playlistResponse = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+        if (searchData.tracks.items.length > 0) {
+            let trackId = searchData.tracks.items[0].id;
+            trackUris.push(`spotify:track:${trackId}`);
+            console.log(`Found: ${song.name} - ${song.artist}`);
+        } else {
+            console.log(`Not Found: ${song.name} - ${song.artist}`);
+        }
+    }
+
+    // 4. Add All Found Tracks to Playlist
+    if (trackUris.length > 0) {
+        let addTrackResponse = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
             method: "POST",
             headers: {
                 Authorization: `Bearer ${accessToken}`,
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                name: "Imported Songs",
-                description: "A playlist for imported songs",
-                public: false, // Set to true if you want it public
+                uris: trackUris, // List of track URIs
             }),
         });
 
-        let playlistData = await playlistResponse.json();
-        let playlistId = playlistData.id;
-        console.log("Playlist Created:", playlistData.name);
-
-        // 3. Search for Tracks and Add to Playlist
-        let trackUris = [];
-
-        for (let song of songList) {
-            let query = `track:${song.name} album:${song.album} artist:${song.artist}`;
-            let encodedQuery = encodeURIComponent(query);
-
-            let searchResponse = await fetch(`https://api.spotify.com/v1/search?q=${encodedQuery}&type=track&limit=1`, {
-                method: "GET",
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                    "Content-Type": "application/json",
-                },
-            });
-
-            let searchData = await searchResponse.json();
-
-            if (searchData.tracks.items.length > 0) {
-                let trackId = searchData.tracks.items[0].id;
-                trackUris.push(`spotify:track:${trackId}`);
-                console.log(`Found: ${song.name} - ${song.artist}`);
-            } else {
-                console.log(`Not Found: ${song.name} - ${song.artist}`);
-            }
-        }
-
-        // 4. Add All Found Tracks to Playlist
-        if (trackUris.length > 0) {
-            let addTrackResponse = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    uris: trackUris, // List of track URIs
-                }),
-            });
-
-            if (addTrackResponse.ok) {
-                console.log("Songs added to playlist successfully!");
-            } else {
-                console.error("Failed to add songs:", addTrackResponse.statusText);
-            }
+        if (addTrackResponse.ok) {
+            console.log("Songs added to playlist successfully!");
         } else {
-            console.log("No valid songs found to add.");
+            console.error("Failed to add songs:", addTrackResponse.statusText);
         }
-    } catch (error) {
-        console.error("Error:", error);
-    }*/
+    } else {
+        console.log("No valid songs found to add.");
+    }
 
-    fetch('https://api.spotify.com/v1/me/tracks', {
+    /*fetch('https://api.spotify.com/v1/me/tracks', {
     method: 'PUT',
     headers: {
         Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
     },
     body: JSON.stringify({ ids: ['3n3Ppam7vgaVa1iaRUc9Lp'] }),
+    body: JSON.stringify({ ids: ['4A65rF6f8pCaAvYqgcfaWN'] }),
     })
     .then((response) => {
         if (response.ok) {
@@ -167,5 +164,6 @@ async function createPlaylistAndAddSongs(songList) {
         console.error('Failed to add song:', response.statusText);
         }
     })
-    .catch(console.error);
+    .catch(console.error);*/
+
 }
